fcfd7008fb66d7b9ff3b5f808f783f2d0239dc7c
Victor ter Hark <victor@afurastore.com>
2022-05-03 16:51:16 +0200

    Zeitwerk

--- app/subscribers/spree/vend_brand_subscriber.rb
+++ app/subscribers/spree/vend_brand_subscriber.rb
@@ -16,3 +16,31 @@ module Spree
 
    end
  end
+
+#  module Spree
+#    module VendVariantSubscriber
+#       include Spree::Event::Subscriber
+   
+#       RESOURCES.each do |resource|
+#          event_action :resource_created, event_name: :sync_resource
+#          event_action :resource_updated, event_name: :sync_resource
+#          event_action :resource_destroyed, event_name: :destroy_resource
+#       end
+   
+#       def sync_resource(event)
+#          resource = event.payload[:payload]
+#          request = SolidusVend::SyncResource::RESOURCE.call(variant)
+         
+#          if request.success
+#             # variant.update(vend_last_sync: Time.now)
+#             resource.update(vend_id: request.response.id) if !resource.vend_id
+#          end
+#       end
+
+#       def destroy_resource(event)
+#          return if !event.payload[:payload].vend_id
+#          Vend::RESOURCE.destroy(event.payload[:payload].vend_id)
+#       end
+
+#    end
+#  end
